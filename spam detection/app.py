a7a2b9da7e1851b3751c22e8e9da01e088f82bd3
Tanishk511 <115484119+Tanishk511@users.noreply.github.com>
2023-04-07 22:52:42 +0530

    Update app.py

--- spam detection/app.py
+++ spam detection/app.py
@@ -5,20 +5,28 @@ import pickle
 
 flask_app = Flask(__name__)
 model = pickle.load(open("spam-sms-mnb-model.pkl","rb"))
-cv = pickle.load(open('cv-transform.pkl','rb'))
+trans = pickle.load(open('cv-transform.pkl','rb'))
 
 @flask_app.route("/")
 def Home():
+    return render_template("login.html")
+
+@flask_app.route("/mails", methods= ["POST"])
+def mails():
     return render_template("index.html")
+
+@flask_app.route("/messages", methods= ["POST"])
+def messages():
+   return render_template("index.html")
 
 @flask_app.route("/predict", methods= ["POST"])
 def predict():
     if request.method == 'POST':
-     message = request.form['mail']
-     data = [message]
-     float_features = cv.transform(data).toarray()
+     mail = request.form['mail']
+     data = [mail]
+     float_features = trans.transform(data).toarray()
      my_prediction = model.predict(float_features)
-     return render_template("result.html", prediction = my_prediction)
-
+     return render_template("index.html", prediction = my_prediction)
+    
 if __name__ == "__main__":
     flask_app.run(debug=True)
